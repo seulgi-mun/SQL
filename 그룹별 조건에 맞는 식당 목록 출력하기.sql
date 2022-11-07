d0f17eb56dc321c56ab6da78ab90467dd0af0fd6
문슬기 <tmfrlghtjr@gmail.com>
2022-11-07 16:23:26 +0900

    sql prac

---
+++ 그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -0,0 +1,15 @@
+"""
+MEMBER_PROFILE와 REST_REVIEW 테이블에서 리뷰를 가장 많이 작성한 회원의 리뷰들을 조회하는 SQL문을 작성해주세요.
+회원 이름, 리뷰 텍스트, 리뷰 작성일이 출력되도록 작성해주시고, 결과는 리뷰 작성일을 기준으로 오름차순, 리뷰 작성일이 같다면 리뷰 텍스트를 기준으로 오름차순 정렬해주세요.
+"""
+
+SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM REST_REVIEW R
+JOIN (SELECT RR.MEMBER_ID, M.MEMBER_NAME, RANK() OVER(ORDER BY CNT DESC) RANKING
+      FROM (SELECT *, COUNT(MEMBER_ID) AS CNT
+           FROM REST_REVIEW
+           GROUP BY MEMBER_ID) AS RR
+      JOIN MEMBER_PROFILE M ON RR.MEMBER_ID = M.MEMBER_ID) M
+ON M.MEMBER_ID = R.MEMBER_ID
+WHERE M.RANKING = 1
+ORDER BY R.REVIEW_DATE
