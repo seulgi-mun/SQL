9602c5e718d24f6d85e87ea0efd6e8cec872a2a3
문슬기 <tmfrlghtjr@gmail.com>
2023-01-11 00:12:13 +0900

    sql prac

---
+++ 없어진 기록 찾기.sql
@@ -0,0 +1,8 @@
+"""
+입양을 간 기록은 있는데,
+보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+"""
+
+SELECT O.ANIMAL_ID, O.NAME FROM ANIMAL_OUTS O
+LEFT OUTER JOIN ANIMAL_INS I ON I.ANIMAL_ID = O.ANIMAL_ID 
+WHERE I.ANIMAL_ID IS NULL
