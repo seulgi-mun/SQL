9ff67a6bc66475aceb7b8cb00423481fcc53ca14
문슬기 <tmfrlghtjr@gmail.com>
2023-01-21 22:59:02 +0900

    sql prac

---
+++ 오랜 기간 보호한 동물(2).sql
@@ -0,0 +1,10 @@
+"""
+입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리의 아이디와 이름을 조회하는 SQL문을 작성해주세요.
+이때 결과는 보호 기간이 긴 순으로 조회해야 합니다.
+"""
+
+SELECT O.ANIMAL_ID, O.NAME FROM ANIMAL_OUTS O
+RIGHT JOIN ANIMAL_INS I ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE DATEDIFF(O.DATETIME, I.DATETIME)
+ORDER BY DATEDIFF(O.DATETIME, I.DATETIME) DESC
+LIMIT 2
